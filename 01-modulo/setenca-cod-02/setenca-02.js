84e7b70c77f71f8bceae6a0843032357c15e2be2
wildsonromeu <wildsonromeu@gmail.com>
2022-07-23 22:20:36 -0300

    Setença de código 02

---
+++ 01-modulo/setenca-cod-02/setenca-02.js
@@ -0,0 +1,18 @@
+/*Setenças que executam textos na tela*/
+console.log('O JS pode usar aspas simples para Strings');
+console.log();
+
+/*Posso ter várias setenças*/
+
+console.log("Passo #1");
+console.log("Passo #2");
+console.log("Passo #3");
+console.log();
+
+/*A ordem das setenças é super importante no código!*/
+
+console.log("Passo #1");
+console.log("depois...");
+console.log("Passo #2");
+console.log("depois...");
+console.log("Passo #3");
