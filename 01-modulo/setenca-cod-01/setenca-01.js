f52a33af0082c2460c27276d87d39f3e2464a5ee
wildsonromeu <wildsonromeu@gmail.com>
2022-07-23 22:05:08 -0300

    Setença de Código 01

---
+++ 01-modulo/setenca-cod-01/setenca-01.js
@@ -0,0 +1,4 @@
+/*Em JS uma setença pode ou não termina com um ponto-e-virgula
+*/ 
+
+console.log("Aqui tem uma setença de código: console.log(Strings)"); /*Essa linha representa uma setença de código!*/
