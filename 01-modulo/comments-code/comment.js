b1816cc5674392cd9231c102d706b23e8ccd77c7
wildsonromeu <wildsonromeu@gmail.com>
2022-07-27 22:19:26 -0300

    Comentar Código

---
+++ 01-modulo/comments-code/comment.js
@@ -0,0 +1,7 @@
+/*Comentário serve para argumentar sobre algum trecho de código escrito*/
+
+let var1 = 10; //Esta variável faz parte de uma lógica x
+
+/*Comentando código*/
+
+//console.log(var1); Esse trecho de cósigo não será copilado.
