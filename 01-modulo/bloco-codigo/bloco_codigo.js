24af2d3202a6a0da8824e5fe25247479a2416bd9
wildsonromeu <wildsonromeu@gmail.com>
2022-07-27 22:31:44 -0300

    Bloco de Código

---
+++ 01-modulo/bloco-codigo/bloco_codigo.js
@@ -0,0 +1,32 @@
+/*Introdução a bloco de código*/
+
+/*Posso criar um bloco dentro do outro*/
+
+/*
+
+{
+        1° BLOCO_CODIGO
+{
+        2° BLOCO_CODIGO
+    {
+        3° BLOCO_CODIGO
+    }
+}
+
+
+}
+
+
+*/
+
+{
+    console.log("1° BLOCO_CODIGO");
+
+        {
+            console.log(" 2° BLOCO_CODIGO");
+
+                {
+                    console.log("   3° BLOCO_CODIGO");
+                }
+        }
+}
