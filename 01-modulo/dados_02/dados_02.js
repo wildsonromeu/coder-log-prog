f99ca5dec0638b3f3552e6329422dcf9f305c1a0
wildsonromeu <wildsonromeu@gmail.com>
2022-08-05 00:16:12 -0300

    Conceito e Práticas com Variáveis.

---
+++ 01-modulo/dados_02/dados_02.js
@@ -0,0 +1,11 @@
+// Toda variável tem seu identifcador, que é o seu nome.
+// É importante definir bons nomes para pastas, arquivos e variáveis.
+// Posso atribuir um determinado valor para variável ou não.
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1-desconto);
+
+console.log("Desconto = " + preco * desconto);
+console.log("Valor com desconto aplicado = " + precoComDesconto);
+
