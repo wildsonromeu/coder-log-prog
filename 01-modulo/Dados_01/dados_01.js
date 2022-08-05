91e17a031a98b48737ab123ed8a44fe0c18c9829
wildsonromeu <wildsonromeu@gmail.com>
2022-08-04 23:58:30 -0300

    Trabalhando com Dados #01

---
+++ 01-modulo/Dados_01/dados_01.js
@@ -0,0 +1,15 @@
+// O código sem um próposito se torna um valor literal, ou seja, não faz sentido dentro da lógica que preciso para desenvolvedor um Software.
+// Entretanto, temos o conceito de variáveis que nos permite a cada dado colocado no código, faça com que tudo tenho uma lógico dentro do desenvolvimento dde um Software.
+
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log("Preço Final = " + precoFinal);
+
