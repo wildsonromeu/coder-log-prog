55c16d84a055404137a47915aa4fe79aefd85d51
wildsonromeu <wildsonromeu@gmail.com>
2022-09-17 23:25:01 -0300

    Desafio: Calcular Área da Circuferência

---
+++ 01-modulo/Desafio_AreaCircuf/areaCIrcuf.js
@@ -0,0 +1,15 @@
+// Calcular Área da Circuferência
+
+/*
+    Fórmula:
+
+        A = pi * (r * r)
+*/
+
+const pi = 3.14;
+
+let raio = 10;
+
+let areaCirc = pi * (raio * raio);
+
+console.log("Área da Circunferência é = " + areaCirc);
