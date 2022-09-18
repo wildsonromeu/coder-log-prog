450352fb4b5137f26775f492a11c8a7f0a7d4172
wildsonromeu <wildsonromeu@gmail.com>
2022-09-17 23:13:26 -0300

    Conceito e Práticas com Variáveis

---
+++ 01-modulo/Dados_03/dados_03.js
@@ -0,0 +1,24 @@
+/*Tipos de Dados
+
+- String
+
+- Number
+
+- JS não faz diferença entre Inteiros ou com ponto flutuante
+
+- Boolean
+
+*/
+
+// Para saber o tipo da variável
+
+let salario = 1020.51
+
+console.log(typeof salario);
+
+let falsoOuVerdadeiro = false;
+
+console.log(typeof falsoOuVerdadeiro);
+
+
+
